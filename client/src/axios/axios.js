1d2a4655afe994c13774f321f80673b279bd0a40
Mufadal03 <sadrimufadal@gmail.com>
2023-03-15 08:42:38 +0530

    base url changed to deployed backend

--- client/src/axios/axios.js
+++ client/src/axios/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const customAxios = axios.create({
-    baseURL:"http://localhost:8080"
+    // baseURL:"http://localhost:8080".
+    baseURL:'https://pococare-seven.vercel.app'
 })
 
 customAxios.interceptors.response.use((res) => {
